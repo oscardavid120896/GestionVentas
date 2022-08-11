77ced56303cbfaba3aa7a0f0a940001f1ba04296
OscarCastanedaR <pasante.desarrollo09@nadglobal.com>
2022-08-11 15:29:26 -0500

    asqas

--- public/js/js_directivo/directivoGrupo.js
+++ public/js/js_directivo/directivoGrupo.js
@@ -86,7 +86,7 @@ function nuevoG(){
                 headers: {
                   'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
                   },
-                url: '/nuevoG/',
+                url: 'https://tranquil-mountain-72526.herokuapp.com/nuevoG/',
                 method: 'post',
                 data:{
                     nombre: nombre,
