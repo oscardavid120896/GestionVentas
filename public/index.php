97b22bc236baa18dabfd66724d86d0c4dfe7cfbc
OscarCastanedaR <pasante.desarrollo09@nadglobal.com>
2022-08-11 18:30:24 -0500

    a

--- public/index.php
+++ public/index.php
@@ -4,6 +4,12 @@ use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
+header('Access-Control-Allow-Headers: token, Content-Type');
+header('Access-Control-Max-Age: 1728000');
+header('Content-Length: 0');
+header('Content-Type: text/plain');
 
 /*
 |--------------------------------------------------------------------------
