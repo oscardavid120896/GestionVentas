45e94c8733871ad019e62c0f6424ba03cfc3a5c3
OscarCastanedaR <pasante.desarrollo09@nadglobal.com>
2022-08-12 11:23:21 -0500

    a

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,6 +15,7 @@
 		</div>
 		<div class="login-content">
 			<form method="POST" action="{{ route('login') }}" id="login">
+            @csrf
 				<img src="../img/profile.svg">
 				<h2 class="title">Bienvenido</h2>
                 @error('email')
