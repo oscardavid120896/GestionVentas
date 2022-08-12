27a4dd52933e61d6170110149df4c958a9fc8cb3
OscarCastanedaR <pasante.desarrollo09@nadglobal.com>
2022-08-11 19:11:33 -0500

    a

--- public/index.php
+++ public/index.php
@@ -8,7 +8,6 @@ header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 header("Allow: GET, POST, OPTIONS, PUT, DELETE");
-headers('X-CSRF-TOKEN: $("meta[name="csrf-token"]").attr("content")');
 $method = $_SERVER['REQUEST_METHOD'];
 if($method == "OPTIONS") {
     die();
