ef91396d292916fe161cf5638946397ba2cfd8f9
OscarCastanedaR <pasante.desarrollo09@nadglobal.com>
2022-08-11 22:23:22 -0500

    a

--- public/index.php
+++ public/index.php
@@ -4,14 +4,6 @@ use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
-header('Access-Control-Allow-Origin: *');
-header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
-header("Allow: GET, POST, OPTIONS, PUT, DELETE");
-$method = $_SERVER['REQUEST_METHOD'];
-if($method == "OPTIONS") {
-    die();
-}
 
 /*
 |--------------------------------------------------------------------------
