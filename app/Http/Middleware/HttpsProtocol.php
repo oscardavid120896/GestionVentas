e08f5cc2ce88df3a0d82f82a9531f80dc8c88c90
OscarCastanedaR <pasante.desarrollo09@nadglobal.com>
2022-08-12 04:37:06 -0500

    a

--- app/Http/Middleware/HttpsProtocol.php
+++ app/Http/Middleware/HttpsProtocol.php
@@ -9,12 +9,12 @@ class HttpsProtocol
 {
 
     public function handle(Request $request, Closure $next){
-     /*   return $next($request)
+        return $next($request)
        //Url a la que se le dará acceso en las peticiones
       ->header("Access-Control-Allow-Origin", "https://tranquil-mountain-72526.herokuapp.com")
       //Métodos que a los que se da acceso
       ->header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
       //Headers de la petición
       ->header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, X-Token-Auth, Authorization"); 
-    */}
+    }
 }
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -66,7 +66,7 @@
             </form>
         </div>
     </div>
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
     <script type="text/javascript" src="../js/main.js"></script>
     <script type="text/javascript" src="../js/validaciones.js"></script>
     
