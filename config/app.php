6cb8852ef12c26a35b40f32e62c8744b0b4cae19
OscarCastanedaR <pasante.desarrollo09@nadglobal.com>
2022-08-12 03:40:53 -0500

    a

--- config/app.php
+++ config/app.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://coral-app-azql2.ondigitalocean.app/'),
 
     'asset_url' => env('ASSET_URL'),
 
